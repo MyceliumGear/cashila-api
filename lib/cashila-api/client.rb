155a8892807de415333090d8c2bda9a8b88f7d6e
Eugene Zolotarev <eugzol@gmail.com>
2016-02-29 14:23:40 +0000

    Cosmetic changes: json -> body

--- lib/cashila-api/client.rb
+++ lib/cashila-api/client.rb
@@ -45,13 +45,13 @@ module CashilaAPI
     # @param [String] email
     # @param [Hash] details keys: first_name, last_name, address, postal_code, city, country_code
     def create_account_or_login(email:, details: nil, password: nil, second_factor: nil)
-      json = {account: {email: email}}
-      json[:account][:password] = password if password
-      json[:account][:second_factor] = second_factor if second_factor
-      json[:verification] = details if details
+      body = {account: {email: email}}
+      body[:account][:password] = password if password
+      body[:account][:second_factor] = second_factor if second_factor
+      body[:verification] = details if details
 
       response = connection(sign: true).put('/api/v1/account') do |req|
-        req.body = MultiJson.dump(json)
+        req.body = MultiJson.dump(body)
       end
       parse_response(response)
     end
